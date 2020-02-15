5b4aec702fe2a1d9b1d87beb92ade4fd6ed6405f
Ankor-sys <sebasmoreira4@gmail.com>
2020-02-15 13:44:29 -0600

    Agregado: Calculo Igss e Isr. Modificacion Variables y columnas

---
+++ SebastianMoreira/NOMINA_DE_SUELDOS/src/Empleados.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SEBAS
+ */
+public class Empleados {
+     private String strCodigo;
+    private String strNombreEmpleado, strApellidoEmpleado;
+    private String strPuesto;
+    private String strSalarioOrdinario, strSalarioExtraordinario;
+    private String strComisiones, strBonificaciones, strOtrosDevengados;
+    private String strIgss, strIsr, strAnticipo, strDescuentosJudiciales, strOtrosDescuentos;
+
+    //Constructor
+    public Empleados(){
+    
+    }
+    //Getters y Setters
+    public String getStrCodigo() {
+        return strCodigo;
+    }
+
+    public void setStrCodigo(String intCodigo) {
+        this.strCodigo = intCodigo;
+    }
+
+    public String getStrNombreEmpleado() {
+        return strNombreEmpleado;
+    }
+
+    public void setStrNombreEmpleado(String strNombreEmpleado) {
+        this.strNombreEmpleado = strNombreEmpleado;
+    }
+
+    public String getStrApellidoEmpleado() {
+        return strApellidoEmpleado;
+    }
+
+    public void setStrApellidoEmpleado(String strApellidoEmpleado) {
+        this.strApellidoEmpleado = strApellidoEmpleado;
+    }
+
+    public String getStrPuesto() {
+        return strPuesto;
+    }
+
+    public void setStrPuesto(String strPuesto) {
+        this.strPuesto = strPuesto;
+    }
+
+    public String getStrSalarioOrdinario() {
+        return strSalarioOrdinario;
+    }
+
+    public void setStrSalarioOrdinario(String fltSalarioOrdinario) {
+        this.strSalarioOrdinario = fltSalarioOrdinario;
+    }
+
+    public String getStrSalarioExtraordinario() {
+        return strSalarioExtraordinario;
+    }
+
+    public void setStrSalarioExtraordinario(String fltSalarioExtraordinario) {
+        this.strSalarioExtraordinario = fltSalarioExtraordinario;
+    }
+
+    public String getStrComisiones() {
+        return strComisiones;
+    }
+
+    public void setStrComisiones(String fltComisiones) {
+        this.strComisiones = fltComisiones;
+    }
+
+    public String getStrBonificaciones() {
+        return strBonificaciones;
+    }
+
+    public void setsTRBonificaciones(String fltBonificaciones) {
+        this.strBonificaciones = fltBonificaciones;
+    }
+
+    public String getStrOtrosDevengados() {
+        return strOtrosDevengados;
+    }
+
+    public void setStrOtrosDevengados(String fltOtrosDevengados) {
+        this.strOtrosDevengados = fltOtrosDevengados;
+    }
+
+    public String getStrIgss() {
+        return strIgss;
+    }
+
+    public void setStrIgss(String fltIgss) {
+        this.strIgss = fltIgss;
+    }
+
+    public String getStrIsr() {
+        return strIsr;
+    }
+
+    public void setStrIsr(String fltIsr) {
+        this.strIsr = fltIsr;
+    }
+
+    public String getStrAnticipo() {
+        return strAnticipo;
+    }
+
+    public void setStrAnticipo(String fltAnticipo) {
+        this.strAnticipo = fltAnticipo;
+    }
+
+    public String getStrDescuentosJudiciales() {
+        return strDescuentosJudiciales;
+    }
+
+    public void setStrDescuentosJudiciales(String fltDescuentosJudiciales) {
+        this.strDescuentosJudiciales = fltDescuentosJudiciales;
+    }
+
+    public String getStrOtrosDescuentos() {
+        return strOtrosDescuentos;
+    }
+
+    public void setStrOtrosDescuentos(String fltOtrosDescuentos) {
+        this.strOtrosDescuentos = fltOtrosDescuentos;
+    }
+
+   
+    
+}
